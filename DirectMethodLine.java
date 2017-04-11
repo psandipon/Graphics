4dff2654e19df40d40cab73db37743b9d8ca315b
Sandipon Paul <psandipon@gmail.com>
2017-04-11 22:59:04 +0600

    Add files via upload

---
+++ DirectMethodLine.java
@@ -0,0 +1,70 @@
+/*GRAPHICS HOMEWORK 2*/
+
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.awt.GLCanvas;
+import javax.swing.JFrame;
+
+public class DirectMethodLine implements GLEventListener{
+	
+	static GLProfile profile = GLProfile.get(GLProfile.GL2);
+    static GLCapabilities capabilities = new GLCapabilities(profile);
+    // The canvas 
+    static GLCanvas glcanvas = new GLCanvas(capabilities);
+    
+   public static void main(String[] args) {
+	      //getting the capabilities object of GL2 profile
+	   	  
+	      
+	      DirectMethodLine l = new DirectMethodLine();
+	      //creating frame
+	      glcanvas.addGLEventListener(l);
+	      glcanvas.setSize(600, 400);
+	      
+	      final JFrame frame = new JFrame ("straight Line");
+	      //adding canvas to frame
+	      frame.getContentPane().add(glcanvas);
+	      frame.setSize(frame.getContentPane().getPreferredSize());
+	      frame.setVisible(true);
+	      
+	   }
+   public void display(GLAutoDrawable drawable) {
+      final GL2 gl = drawable.getGL().getGL2();
+       	  gl.glBegin (GL2.GL_POINTS);//static field
+          
+       	  float starTx = 0;
+       	  float enDx = 0.5f;
+       	  float starTy = 0.5f;
+       	  float enDy = 1;
+       	  float b = .001f;
+       	  float m = 0;
+       	  while(starTx < enDx){
+       		gl.glVertex2d(starTx,starTy);  
+       		m = (enDy-starTy)/(enDx-starTx);
+       		starTx+=b;  
+       		starTy=starTy*m+b;
+       	  }
+          
+          gl.glEnd();
+          
+      
+   }
+   
+   public void dispose(GLAutoDrawable arg0) {
+      //method body
+   }
+
+   
+   public void init(GLAutoDrawable drawable) {
+      // method body
+	   //4. drive the display() in a loop
+	    }
+   
+   public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
+      // method body
+   }
+   //end of main
+}//end of classimport javax.media.opengl.GL2;
